31ae8d367b43b07e5a60f4968cd0c6a5c60df036
yohagos <yosef.hagos@googlemail.com>
2022-07-31 16:35:43 +0200

    fixed cors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+export interface Employee {
+  lastname: string;
+  firstname: string;
+  city: string;
+  age: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gitaction-fe';
+
+  infoGET: any;
+
+  lastname = new FormControl('');
+  firstname = new FormControl('');
+  city = new FormControl('');
+  age = new FormControl('');
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit() {}
+
+  createObject() {
+    const emp: Employee = {
+      lastname: this.lastname.value,
+      firstname: this.firstname.value,
+      city: this.city.value,
+      age: +this.age.value
+    }
+    return emp;
+  }
+
+  getBackendInfo() {
+    this.http.get<string>(environment.baseUrl + '/get').subscribe(response => {
+      this.infoGET =  response;
+    });
+  }
+
+  postBackend() {
+    let newEmp = this.createObject()
+    this.http.post(environment.baseUrl + '/post', newEmp).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error)
+    });
+  }
+
 }
